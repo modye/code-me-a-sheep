6edb409f94a8c075b02a5bed7499550890abdb49
Ludovic Boutros <boutrosl@gmail.com>
2019-02-22 16:52:19 +0100

    fix(search): add a unit test for simple searching

--- src/test/java/com/code/a/sheep/codeasheep/CodeASheepApplicationTests.java
+++ src/test/java/com/code/a/sheep/codeasheep/CodeASheepApplicationTests.java
@@ -1,18 +1,33 @@
 package com.code.a.sheep.codeasheep;
 
+<<<<<<< HEAD
 import com.code.a.sheep.codeasheep.domain.SearchResult;
+=======
+import com.code.a.sheep.codeasheep.lucene.LuceneDocumentSearcher;
+import org.apache.lucene.queryparser.flexible.core.QueryNodeException;
+import org.apache.lucene.search.TopDocs;
+>>>>>>> fix(search): add a unit test for simple searching
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+<<<<<<< HEAD
+=======
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+>>>>>>> fix(search): add a unit test for simple searching
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CodeASheepApplicationTests {
 
+<<<<<<< HEAD
     @Autowired
     private DocumentSearcher documentSearcher;
 
@@ -22,6 +37,18 @@ public class CodeASheepApplicationTests {
 
         assertEquals(5, result.getNbHits());
     }
+=======
+	@Autowired
+	private DocumentSearcher documentSearcher;
+
+	@Test
+	public void simpleSearch() throws IOException, QueryNodeException {
+		// TODO should be generic (Not Lucene only)
+		TopDocs result = documentSearcher.searchDocuments("text:boas");
+
+		assertEquals(5, result.totalHits);
+	}
+>>>>>>> fix(search): add a unit test for simple searching
 
 }
 
